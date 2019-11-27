0362a55ea06de1af41d06b778813ccf3180b3e82
Hilda <rudenkomarina2012@gmail.com>
2019-11-27 14:01:14 +0200

    hw2

---
+++ hw2.php
@@ -0,0 +1,170 @@
+<? php;
+echo " Task 1."; echo "<br>";
+
+$a = 3;
+echo $a; echo "<br>";
+echo "<br>";
+
+
+$a1=10;
+$b1=2;
+echo $a1 * $b1; echo "<br>";
+echo $a1 / $b1; echo "<br>";
+echo $a1 + $b1; echo "<br>";
+echo $a1 - $b1; echo "<br>";
+echo "<br>";
+
+
+$c=15;
+$d=2;
+$result=$c+$d;
+echo $result; echo "<br>";
+echo "<br>";
+
+
+$a2=10;
+$b2=2;
+$c2=5;
+$d2= $a2+$b2+$c2;
+echo $c2; echo "<br>";
+echo "<br>";
+
+
+$a3=17;
+$b3=10;
+$c3= $a3-$b3;
+$d3=7;
+$result3= $c3+$d3;
+echo $result3; echo "<br>";
+echo "<br>";
+
+
+echo " Task 2."; echo "<br>";
+
+
+$text= 'Привет, Мир!';
+echo $text; echo "<br>";
+echo "<br>";
+
+
+$text1= 'Привет, ';
+$text2= ' Мир!';
+$privetmir = "$text1 $text2" ;
+echo $privetmir; echo "<br>";
+echo "<br>";
+
+
+echo " Task 3."; echo "<br>";
+$sec=60;
+$honour=60 * $sec; echo $honour; echo "<br>";
+$day=24 * $honour; echo $day; echo "<br>";
+$week=7 * $day; echo $week; echo "<br>";
+$month=30 * $week; echo $month; echo "<br>";
+echo "<br>";
+
+
+echo " Task 4."; echo "<br>";
+
+$var = 1;
+$var += 12;
+$var -= 14;
+$var *= 5;
+$var /= 7;
+$var += 1;
+$var -= 1;
+echo $var;echo "<br>";
+echo "<br>";
+
+
+echo " Task 5."; echo "<br>";
+
+$hon=0;
+$min=26;
+$s=20;
+echo "$hon:$min:$s"; echo "<br>";
+echo "<br>";
+
+
+echo " Task 6."; echo "<br>";
+$text = 'Я';
+$text .= ' хочу' ;
+$text .= ' знать' ;
+$text .= ' PHP!' ;
+echo $text; echo "<br>";
+echo "<br>";
+
+
+echo " Task 7."; echo "<br>";
+ 
+$bar = 10;
+$foo = $bar;
+echo $foo;echo "<br>";
+echo "<br>";
+
+
+echo " Task Еще немного арифметики посложнее  :)."; echo "<br>";
+$e=16;
+$f=24;
+$g = $e + $f;
+$h = $e * $f;
+echo $g; echo "<br>";
+echo $h; echo "<br>";
+echo "<br>";
+
+
+$j = sqrt($e) + sqrt($f);
+echo $j; echo "<br>";
+echo "<br>";
+
+
+$k = ($a3+$b3+$d3)/3;
+echo $k; echo "<br>";
+echo "<br>";
+
+
+$x = 20;
+$y = 30;
+$z = 40; 
+$w = ($x + 1) - 2*($z - 2*($x + $y));
+echo $w; echo "<br>";
+echo "<br>";
+
+
+$y1 = $y / 3;
+echo $y1; echo "<br>";
+$y2 = $y / 5;
+echo $y2; echo "<br>";
+echo "<br>";
+
+
+$x1 = (($x / 100) *30)+$x;
+echo $x1; echo "<br>";
+$x2 = (($x / 100) *120)+$x;
+echo $x2; echo "<br>";
+echo "<br>";
+
+
+$x3 = (40/100) * $x; echo $x3;echo "<br>";
+$y3 = (84/100) * $y; echo $y3;echo "<br>";
+echo $x3 + $y3; echo "<br>";
+echo "<br>";
+
+
+$m = 658;
+echo $m; echo "<br>";
+$m = 6+5+8;
+echo $m; echo "<br>";
+echo "<br>";
+
+
+$t = 5;
+$r = 2; 
+$i = 4;
+$tri = "$t$r$i";
+echo $tri;
+echo "<br>";
+$r = 0;
+echo "$i$r$t";
+
+
+?>
